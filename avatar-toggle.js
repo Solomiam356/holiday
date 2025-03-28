874b22de4b90a8b830e91a3802925d677dbbf26e
Solomiam356 <solomiam356@gmail.com>
2025-03-28 14:06:18 +0200

    Initial commit

---
+++ avatar-toggle.js
@@ -0,0 +1,14 @@
+// avatar-toggle.js
+const image = document.getElementById('avatar');
+
+image.addEventListener('click', function () {
+  if (image.src.match('./assets/santa.png')) {
+    image.src = './assets/reindeer.png';
+  } else if (image.src.match('./assets/reindeer.png')) {
+    image.src = './assets/bear.png';
+  } else if (image.src.match('./assets/bear.png')) {
+    image.src = './assets/cookie.png';
+  } else {
+    image.src = './assets/santa.png';
+  }
+});
